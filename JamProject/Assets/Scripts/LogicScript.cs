eb2e3bcffd1955decd2eff8af35c299d03dae7ee
MisterNo0ne <miktn07@gmail.com>
2025-07-31 01:52:13 -0600

    Added some documentation

--- JamProject/Assets/Scripts/LogicScript.cs
+++ JamProject/Assets/Scripts/LogicScript.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class LogicScript : MonoBehaviour
 {
-    public float maxSwapDistance = 4f; //the maximum range you can switch characters
+    public float maxSwapDistance = 4f; // the maximum range you can switch characters
     public GameObject blueAgent;
     public GameObject redAgent;
     public GameObject yellowAgent;
@@ -16,18 +16,18 @@ public class LogicScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) //left mouse button pressed
+        if (Input.GetMouseButtonDown(0)) // left mouse button pressed
         {
             PlayerController selectedScript = selectedAgent.GetComponent<PlayerController>();
             selectedScript.updateDestination();
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)) // spacebar pressed
         {
             selectedAgent.transform.Find("Indicator").gameObject.SetActive(true);
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space)) // spacebar released
         {
             selectedAgent.transform.Find("Indicator").gameObject.SetActive(false);
             raycastForNewAgent();
@@ -36,32 +36,23 @@ public class LogicScript : MonoBehaviour
 
     public void raycastForNewAgent()
     {
-        // Get mouse position in world space
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 agentToMouse = (mousePos - transform.position).normalized;
 
-        // Raycast from this agent in the direction of the mouse
         RaycastHit2D hit = Physics2D.Raycast(transform.position, agentToMouse, maxSwapDistance);
 
-        if (hit.collider != null)
+        if (hit.collider != null) //did it hit something?
         {
-            Debug.Log("Hit something");
-
-            // Make sure it's another agent
-            if (hit.collider.CompareTag("Agent") && hit.collider.gameObject != gameObject)
+            if (hit.collider.CompareTag("Agent") && hit.collider.gameObject != gameObject) //another agent?
             {
-                Debug.Log("Hit another agent: " + hit.collider.name);
                 changeSelectedAgent(hit.collider.name);
             }
         }
     }
 
+    //Beware, this function is case sensitive and needs the exact name of the GameObject!
     public void changeSelectedAgent(string newName)
     {
-        ////update indicator
-        //GameObject oldIndicator = selectedAgent.transform.Find("Indicator").gameObject;
-        //oldIndicator.SetActive(false);
-
         switch (newName)
         {
             case "Blue Agent":
@@ -80,5 +71,4 @@ public class LogicScript : MonoBehaviour
 
         Debug.Log("Switched control to " + newName);
     }
-
 }
