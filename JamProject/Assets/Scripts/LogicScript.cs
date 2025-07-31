1cb90761f6e4c5317b73a23039f5028f0bb9a912
MisterNo0ne <miktn07@gmail.com>
2025-07-30 21:54:13 -0600

    Added player switching
    Added the basics of player switching

--- JamProject/Assets/Scripts/LogicScript.cs
+++ JamProject/Assets/Scripts/LogicScript.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class LogicScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public PlayerController blueAgentScript;
+    public PlayerController redAgentScript;
+
+    private string agentSelected;
+
     void Start()
     {
-        
+        //i should set blueAgent and redAgent w code but i lowk forgot how
+        agentSelected = "blue";
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) //left mouse button pressed
+        {
+            //this code is terrible but perfect is the enemy of good
+            switch(agentSelected)
+            {
+                case "blue":
+                    blueAgentScript.updateDestination();
+                    break;
+                case "red":
+                    redAgentScript.updateDestination();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //ik this code is bad but its just placeholder so dw abt it
+            if (agentSelected == "blue") changeSelectedAgent("red");
+            else if (agentSelected == "red") changeSelectedAgent("blue");
+        }
+    }
+
+    public void changeSelectedAgent(string newName)
+    {
+        agentSelected = newName;
+        Debug.Log("Switched control to " + newName);
     }
 }
--- JamProject/Assets/Scripts/PlayerController.cs
+++ JamProject/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,21 @@ public class PlayerController : MonoBehaviour
 {
     public NavMeshAgent agent;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent.updateRotation = false;
         agent.updateUpAxis = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0;
-            agent.SetDestination(mousePos);
-        }
+        
+    }
+
+    public void updateDestination()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        agent.SetDestination(mousePos);
     }
 }
