81b15019e9677a5954b46eec4229da0b87ad32d8
Luca DalCanto <111649024+Luca-Skyline@users.noreply.github.com>
2025-08-01 16:17:47 -0600

    FOOTSTEP SOUNDS!!!

--- JamProject/Assets/Scripts/PlayerController.cs
+++ JamProject/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,40 @@ public class PlayerController : MonoBehaviour
 {
     public NavMeshAgent agent;
 
+    [SerializeField]
+    private AudioClip[] footstepSounds;
+    [SerializeField]
+    private float stepTime = 0.2f; // Time between footsteps
+
+    private AudioSource audioSource;
+    private float stepTimer = 0.19f; // Timer to track when to play the next step sound
+    private Vector3 lastPos;
+
     void Start()
     {
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        audioSource = GetComponent<AudioSource>();
+        lastPos = transform.position;
     }
 
     void Update()
     {
-        
+        if(lastPos != transform.position)
+        {
+            stepTimer += Time.deltaTime;
+            if (stepTimer >= stepTime)
+            {
+                AudioClip audioClip = footstepSounds[Random.Range(0, footstepSounds.Length)];
+                audioSource.PlayOneShot(audioClip);
+                stepTimer -= stepTime; // Reset the timer after playing a sound (kind of)
+            }
+        }
+        else
+        {
+            stepTimer = 0.19f; // Reset timer if not moving
+        }
+        lastPos = transform.position;
     }
 
     public void updateDestination()
