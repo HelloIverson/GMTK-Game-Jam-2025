f00b471ed91e32332834dadd1cf71ca025268556
Luca DalCanto <111649024+Luca-Skyline@users.noreply.github.com>
2025-08-02 13:14:19 -0600

    music controller!

--- JamProject/Assets/Scripts/AudioManager.cs
+++ JamProject/Assets/Scripts/AudioManager.cs
@@ -20,9 +20,13 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     private AudioClip chaseLoop;
     [SerializeField]
-    private AudioSource currentSource;
+    private AudioSource firstSource;
     [SerializeField]
-    private AudioSource nextSource;
+    private AudioSource loopSource;
+    [SerializeField]
+    private AudioSource tempSource1;
+    [SerializeField]
+    private AudioSource tempSource2;
 
 
     public float masterVolume = 0.75f; // Default master volume
@@ -32,6 +36,7 @@ public class AudioManager : MonoBehaviour
     public float fadeTime = 1.0f; // Time to fade between songs
     private float fadeTimer = 0.0f;
     private bool isFading = false;
+    private bool isFadingIn = false;
     private AudioClip nextClipFirst;
     private AudioClip nextClipLoop;
 
@@ -76,36 +81,96 @@ public class AudioManager : MonoBehaviour
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // Your logic here
-        Debug.Log("Scene changed");
-        if (scene.name == "Start Menus")
-        {
-            if (currentSource.clip == null && nextSource.clip == null)
-            {
-                PlaySeamless(menusFirst, menusLoop);
-            }
-            else
-            {
-                isFading = true;
-                fadeTimer = 0.0f;
-                nextClipFirst = menusFirst;
-                nextClipLoop = menusLoop;
-            }
-
-        }
-        else if (scene.name == "Level 1") {
-            isFading = true;
-            fadeTimer = 0.0f;
-            nextClipFirst = suspenseFirst;
-            nextClipLoop = suspenseLoop;
-        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isFading)
+        {
+            fadeTimer += Time.deltaTime;
+            if (fadeTimer >= fadeTime)
+            {
+                fadeTimer = 0f;
+                isFading = false;
+                if (isFadingIn)
+                {
+                    var temp = firstSource;
+                    var temp2 = loopSource;
+                    firstSource = tempSource1;
+                    loopSource = tempSource2;
+                    tempSource1 = temp;
+                    tempSource2 = temp2;
+                    tempSource2.Stop();
+                    tempSource1.Stop();
+                    isFadingIn = false;
+                }
+                else
+                {
+                    PlaySeamless(nextClipFirst, nextClipLoop);
+                    firstSource.volume = 1;
+                    loopSource.volume = 1;
+                }
+            }
+            else
+            {
+                // Adjust volume of current source to fade out
+                float volume = firstSource.volume - (Time.deltaTime / fadeTime);
+                firstSource.volume = volume;
+                loopSource.volume = volume;
+                Debug.Log("Fading out: " + fadeTimer + " / " + fadeTime);
+            }
+            if (isFadingIn)
+            {
+                // Adjust volume of new source to fade in
+                float volume = Mathf.Lerp(0.0f, 1.0f, fadeTimer / fadeTime);
+                tempSource1.volume = volume * musicVolume;
+                tempSource2.volume = volume * musicVolume;
+            }
+        }
+
+
+    }
+
+    public void ToMenusMusic() {
+        isFading = true;
+        nextClipFirst = menusFirst;
+        nextClipLoop = menusLoop;
+    }
+
+    public void ToSuspenseMusic() {
+        isFading = true;
+        nextClipFirst = suspenseFirst;
+        nextClipLoop = suspenseLoop;
+    }
+
+    public void FadeToSuspenseMusic() {
+        tempSource2.volume = 0;
+        tempSource1.volume = 0;
+        isFading = true;
+        isFadingIn = true;
+        var temp = firstSource;
+        var temp2 = loopSource;
+        firstSource = tempSource1;
+        loopSource = tempSource2;
+        PlaySeamless(suspenseFirst, suspenseLoop);
+        firstSource = temp;
+        loopSource = temp2;
+    }
+
+    public void FadeToChaseMusic() {
+        tempSource2.volume = 0;
+        tempSource1.volume = 0;
+        isFading = true;
+        isFadingIn = true;
+        var temp = firstSource;
+        var temp2 = loopSource;
+        firstSource = tempSource1;
+        loopSource = tempSource2;
+        PlaySeamless(chaseFirst, chaseLoop);
+        firstSource = temp;
+        loopSource = temp2;
     }
 
     public void ChangeMasterVolume(float volume)
@@ -138,17 +203,12 @@ public class AudioManager : MonoBehaviour
         double startTime = AudioSettings.dspTime + 0.1;
 
         // Play first clip
-        currentSource.clip = currentClip;
-        currentSource.PlayScheduled(startTime);
+        firstSource.clip = currentClip;
+        firstSource.PlayScheduled(startTime);
 
         // Schedule second clip
-        nextSource.clip = nextClip;
+        loopSource.clip = nextClip;
         double nextStart = startTime + currentClip.length;
-        nextSource.PlayScheduled(nextStart);
-
-        // Optionally: swap sources for future chaining
-        var temp = currentSource;
-        currentSource = nextSource;
-        nextSource = temp;
+        loopSource.PlayScheduled(nextStart);
     }
 }
